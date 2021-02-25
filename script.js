72ad8fb121edca0a78d75f3ee89fe5988dc60693
Andrii Slobodian <73819571+sproandrii@users.noreply.github.com>
2021-02-25 18:33:08 +0200

    some practice with nums and dates

--- script.js
+++ script.js
@@ -4,12 +4,29 @@
 /////////////////////////////////////////////////
 // BANKIST APP
 
+/////////////////////////////////////////////////
 // Data
+
+// DIFFERENT DATA! Contains movement dates, currency and locale
+
 const account1 = {
   owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -17,23 +34,22 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -69,7 +85,9 @@ const displayMovements = function (movements, sort = false) {
     const html = `
     <div class="movements__row">
     <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-    <div class="movements__value">${mov > 0 ? '£' + mov : mov + '£'}</div>
+    <div class="movements__value">${
+      mov > 0 ? '£' + mov.toFixed(2) : mov.toFixed(2) + '£'
+    }</div>
     </div>
     `;
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -78,7 +96,24 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance} GBP`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)} GBP`;
+};
+
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acu, mov) => acu + mov);
+  labelSumIn.textContent = `£${incomes.toFixed(2)}`;
+  const outcomes = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acu, mov) => acu + mov);
+  labelSumOut.textContent = `£${Math.abs(outcomes.toFixed(2))}`;
+  const interest = acc.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter(dep => dep >= 1)
+    .reduce((acu, int) => acu + int, 0);
+  labelSumInterest.textContent = `£${interest.toFixed(2)}`;
 };
 
 const createUserNames = function (accs) {
@@ -100,31 +135,29 @@ const updateUI = function (acc) {
   // Display summary
   calcDisplaySummary(acc);
 };
-const calcDisplaySummary = function (acc) {
-  const incomes = acc.movements
-    .filter(mov => mov > 0)
-    .reduce((acu, mov) => acu + mov);
-  labelSumIn.textContent = `£${incomes}`;
-  const outcomes = acc.movements
-    .filter(mov => mov < 0)
-    .reduce((acu, mov) => acu + mov);
-  labelSumOut.textContent = `£${Math.abs(outcomes)}`;
-  const interest = acc.movements
-    .filter(mov => mov > 0)
-    .map(deposit => (deposit * acc.interestRate) / 100)
-    .filter(dep => dep >= 1)
-    .reduce((acu, int) => acu + int, 0);
-  labelSumInterest.textContent = `£${interest}`;
-};
 
 let currentAccount;
+
+// FAKE ALWAYS LOGGED IN
+currentAccount = account1;
+updateUI(currentAccount);
+containerApp.style.opacity = 100;
+
+const now = new Date();
+const day = `${now.getDate()}`.padStart(2, 0);
+const month = `${now.getMonth() + 1}`.padStart(2, 0);
+const year = now.getFullYear();
+const hour = now.getHours();
+const min = now.getMinutes();
+labelDate.textContent = `${day}/${month}/${year}, ${hour}: ${min}`;
+
 btnLogin.addEventListener('click', function (e) {
   e.preventDefault();
   currentAccount = accounts.find(
     acc => acc.userName === inputLoginUsername.value
   );
   console.log(currentAccount);
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -139,7 +172,7 @@ btnLogin.addEventListener('click', function (e) {
 });
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.userName === inputTransferTo.value
   );
@@ -162,7 +195,7 @@ btnTransfer.addEventListener('click', function (e) {
 
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     currentAccount.movements.push(amount);
   }
@@ -177,7 +210,7 @@ btnClose.addEventListener('click', function (e) {
   console.log('start');
   if (
     currentAccount.userName === inputCloseUsername.value &&
-    currentAccount.pin === Number(inputClosePin.value)
+    currentAccount.pin === +inputClosePin.value
   ) {
     const index = accounts.findIndex(
       acc => acc.userName === currentAccount.userName
@@ -418,54 +451,222 @@ const movements = [200, 450, 0, -400, 3000, -650, -130, 70, 1300];
 // console.log(dices100random);
 
 // 1
-const allDeposit = accounts
-  .flatMap(el => el.movements)
-  .reduce((acu, el) => acu + el, 0);
-console.log(allDeposit);
+// const allDeposit = accounts
+//   .flatMap(el => el.movements)
+//   .reduce((acu, el) => acu + el, 0);
+// console.log(allDeposit);
 
-// 2
-const numDeposit1000 = accounts
-  .flatMap(el => el.movements)
-  .filter(el => el >= 50).length;
-console.log(numDeposit1000);
+// // 2
+// const numDeposit1000 = accounts
+//   .flatMap(el => el.movements)
+//   .filter(el => el >= 50).length;
+// console.log(numDeposit1000);
 
-// Prefixed ++ operator
-// let a = 10;
-// console.log(++a);
+// // Prefixed ++ operator
+// // let a = 10;
+// // console.log(++a);
 
-//3
-const numDep1000withReduce = accounts
-  .flatMap(el => el.movements)
-  .reduce((acuLength, el) => (el >= 50 ? ++acuLength : acuLength), 0);
-console.log(numDep1000withReduce);
+// //3
+// const numDep1000withReduce = accounts
+//   .flatMap(el => el.movements)
+//   .reduce((acuLength, el) => (el >= 50 ? ++acuLength : acuLength), 0);
+// console.log(numDep1000withReduce);
 
-const { deposits, withdrawals } = accounts
-  .flatMap(el => el.movements)
-  .reduce(
-    (sums, cur) => {
-      // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
-      sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
-      return sums;
-    },
-    { deposits: 0, withdrawals: 0 }
-  );
-console.log(deposits, typeof withdrawals, withdrawals);
+// const { deposits, withdrawals } = accounts
+//   .flatMap(el => el.movements)
+//   .reduce(
+//     (sums, cur) => {
+//       // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+//       sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+//       return sums;
+//     },
+//     { deposits: 0, withdrawals: 0 }
+//   );
+// console.log(deposits, typeof withdrawals, withdrawals);
 
-// 4
-// this is a nice title -> This Is a Nice Titel
-const convertTitleCase = function (title) {
-  const exceptions = ['a', 'an', 'the', 'and', 'but', 'or', 'in', 'on', 'with'];
-  const titelCase = title
-    .toLowerCase()
-    .split(' ')
-    .map((word, i) =>
-      exceptions.includes(word) && i !== 0
-        ? word
-        : word[0].toUpperCase() + word.slice(1)
-    )
-    .join(' ');
-  return titelCase;
-};
-console.log(convertTitleCase('this is a nice title'));
-console.log(convertTitleCase('this is a LONG titel but not too long'));
-console.log(convertTitleCase('and here is another titel with an EXAMPLE'));
+// // 4
+// // this is a nice title -> This Is a Nice Titel
+// const convertTitleCase = function (title) {
+//   const exceptions = ['a', 'an', 'the', 'and', 'but', 'or', 'in', 'on', 'with'];
+//   const titelCase = title
+//     .toLowerCase()
+//     .split(' ')
+//     .map((word, i) =>
+//       exceptions.includes(word) && i !== 0
+//         ? word
+//         : word[0].toUpperCase() + word.slice(1)
+//     )
+//     .join(' ');
+//   return titelCase;
+// };
+// console.log(convertTitleCase('this is a nice title'));
+// console.log(convertTitleCase('this is a LONG titel but not too long'));
+// console.log(convertTitleCase('and here is another titel with an EXAMPLE'));
+
+// const dogs = [
+//   { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+//   { weight: 8, curFood: 200, owners: ['Matilda'] },
+//   { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+//   { weight: 32, curFood: 340, owners: ['Michael'] },
+// ];
+// // 1
+// dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
+
+// // 2
+// const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+// console.log(dogSarah);
+// console.log(
+//   `Sarah dog eating too ${
+//     dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
+//   }`
+// );
+// // 3
+// const ownersEatTooMuch = dogs
+//   .filter(dog => dog.curFood > dog.recFood)
+//   .flatMap(dog => dog.owners);
+// .flat()
+// const ownersEatTooLittle = dogs
+//   .filter(dog => dog.curFood < dog.recFood)
+//   .flatMap(dog => dog.owners);
+
+// console.log(ownersEatTooMuch);
+// console.log(ownersEatTooLittle);
+
+// // 4
+// console.log(`${ownersEatTooMuch.join(' and ')}'s eat too much!`);
+// console.log(`${ownersEatTooLittle.join(' and ')}'s eat too little!`);
+
+// // 5
+// console.log(`${dogs.curFood === dogs.recFood ? 'false' : 'true'}`);
+// dogs.some(dog => {
+//   if (dog.curFood === dog.recFood) {
+//     console.log(
+//       `The ${dog.owners.join(
+//         ' and '
+//       )}'s dog eat the same food that recommended!`
+//     );
+//   }
+// });
+
+// // 6
+// const checkEatingOkay = dog =>
+//   dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+
+// // 7
+// console.log(dogs.filter(checkEatingOkay));
+
+// // 8
+// const dogsCopySorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+// console.log(dogsCopySorted);
+
+// function oddOrEven(array) {
+//   const sum = array.reduce((acu, el) => acu + el, 0);
+//   return sum === [] ? 'array with zero' : sum % 2 === 0 ? 'even' : 'odd';
+// }
+// console.log(oddOrEven([0, 1, 5]));
+
+// ----------------------------------------------- SECTION 12 - Numbers, Date, Int and Timer --------------------------------------------------------------
+
+// console.log(23 === 23.0);
+// console.log(0.1 + 0.2 === 0.3);
+
+// // Converting
+// console.log(Number('15'));
+// console.log(+'15');
+
+// // Parsing
+// console.log(Number.parseInt('30px', 10));
+// console.log(Number.parseInt('se30', 10));
+
+// console.log(Number.parseInt('  2.5 rem '));
+// console.log(Number.parseFloat('  2.5 rem '));
+
+// console.log('------');
+// // Check if value is NaN
+// console.log(Number.isNaN(7));
+// console.log(Number.isNaN('7'));
+// console.log(Number.isNaN(+'20w'));
+// console.log(Number.isNaN(70 / 0));
+
+// console.log('------');
+// // Check if value is number
+// console.log(Number.isFinite(7));
+// console.log(Number.isFinite('7'));
+// console.log(Number.isFinite(+'7'));
+// console.log(Number.isFinite(+'73e'));
+// console.log(Number.isFinite(7 / 0));
+
+// console.log('------');
+// console.log(Number.isInteger(5));
+// console.log(Number.isInteger(5.0));
+// console.log(Number.isInteger(5 / 0));
+// console.log(Number.isInteger(6 / 2));
+
+// console.log(Math.sqrt(25));
+// console.log(25 ** (1 / 2));
+// console.log(8 ** (1 / 3));
+
+// console.log(Math.max(13, 34, 2, 4, 74, 2));
+// console.log(Math.max(12, '34'));
+// console.log(Math.max(12, '4px', 5));
+
+// console.log(Math.min(13, 34, 2, 4, 74, 2));
+
+// const roundInt = (max, min) =>
+//   Math.floor(Math.random() * (max - min) + 1) + min;
+// console.log(roundInt(10, 5));
+
+// // Rounding decimals
+// console.log((3.2).toFixed(0));
+// console.log((3.2).toFixed(3));
+// console.log((3.2544).toFixed(2));
+// console.log(+(3.2556).toFixed(2));
+
+// labelBalance.addEventListener('click', function () {
+//   [...document.querySelectorAll('.movements__row')].forEach((row, i) => {
+//     if (i % 2 === 1) row.style.backgroundColor = 'green';
+//     if (i % 2 === 0) row.style.backgroundColor = 'yellow';
+//   });
+// });
+
+// console.log(2 ** 53 - 1);
+// console.log(2 ** 53 + 2);
+// console.log(Number.MAX_SAFE_INTEGER);
+
+// console.log(257349574396064234534645754747n);
+// console.log(BigInt(324325345));
+
+// const bInt = BigInt(324325345);
+// console.log(bInt + " It's realy huge number!");
+
+// Create a date
+// const now = new Date();
+// console.log(now);
+
+// console.log(new Date('Thu Feb 25 2021 13:51:48'));
+// console.log(new Date(' Sep 1, 2021'));
+
+// console.log(new Date(2021, 10, 36));
+
+// console.log(new Date(0));
+// console.log(new Date(3 * 24 * 60 * 60 * 1000));
+// console.log(new Date(3 * 24 * 3600000));
+
+// const future = new Date(2037, 10, 17, 3, 23);
+// console.log(future);
+// console.log(future.getFullYear());
+// console.log(future.getMonth());
+// console.log(future.getDate());
+// console.log(future.getDay());
+// console.log(future.getHours());
+// console.log(future.getSeconds());
+// console.log(future.getMilliseconds());
+// console.log(future.toISOString());
+// console.log(future.getTime());
+
+// console.log(new Date(2142033780000));
+
+// console.log(Date.now());
+
+// future.setFullYear(2039);
+// console.log(future);
